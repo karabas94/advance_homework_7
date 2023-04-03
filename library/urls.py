45370d25764520ae3d7a455c24e40605dde6e681
karabas94 <alkar_94@ukr.net>
2023-04-04 01:45:49 +0300

    -created view and templates

---
+++ library/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'library'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('authors/', views.author_list, name='author_list'),
+    path('authors/<int:pk>/', views.author_detail, name='author_detail'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.book_detail, name='book_detail'),
+    path('publishers/', views.publisher_list, name='publisher_list'),
+    path('publishers/<int:pk>/', views.publisher_detail, name='publisher_detail'),
+    path('stores/', views.store_list, name='store_list'),
+    path('stores/<int:pk>/', views.store_detail, name='store_detail'),
+]
