ddcc5239e2f307c63e89b0a0251d92fcf32d8091
karabas94 <alkar_94@ukr.net>
2023-04-04 18:06:37 +0300

    -used aggregate, annotate, prefetch_related and select_related

--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Author, Publisher, Store
+from django.db.models import Count, Avg
 
 
 def index(request):
@@ -7,40 +8,44 @@ def index(request):
 
 
 def author_list(request):
-    authors = Author.objects.all()
+    authors = Author.objects.prefetch_related('book_set').annotate(num_books=Count('book')).all()
     return render(request, 'library/author_list.html', {'authors': authors})
 
 
 def author_detail(request, pk):
-    author = get_object_or_404(Author, pk=pk)
-    return render(request, 'library/author_detail.html', {'author': author})
+    author = get_object_or_404(Author.objects.prefetch_related('book_set').all(), pk=pk)
+    avg_rating = author.book_set.all().aggregate(Avg('rating'))['rating__avg']
+    return render(request, 'library/author_detail.html', {'author': author, 'avg_rating': avg_rating})
 
 
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.objects.prefetch_related('authors').annotate(num_authors=Count('authors')).all()
     return render(request, 'library/book_list.html', {'books': books})
 
 
 def book_detail(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, 'library/book_detail.html', {'book': book})
+    book = get_object_or_404(Book.objects.select_related('publisher').prefetch_related('authors').all(), pk=pk)
+    num_authors = book.authors.all().aggregate(Count('id'))['id__count']
+    return render(request, 'library/book_detail.html', {'book': book, 'num_authors': num_authors})
 
 
 def publisher_list(request):
-    publishers = Publisher.objects.all()
+    publishers = Publisher.objects.prefetch_related('book_set').annotate(num_books=Count('book')).all()
     return render(request, 'library/publisher_list.html', {'publishers': publishers})
 
 
 def publisher_detail(request, pk):
-    publisher = get_object_or_404(Publisher, pk=pk)
-    return render(request, 'library/publisher_detail.html', {'publisher': publisher})
+    publisher = get_object_or_404(Publisher.objects.prefetch_related('book_set'), pk=pk)
+    avg_price = publisher.book_set.all().aggregate(Avg('price'))['price__avg']
+    return render(request, 'library/publisher_detail.html', {'publisher': publisher, 'avg_price': avg_price})
 
 
 def store_list(request):
-    stores = Store.objects.all()
+    stores = Store.objects.prefetch_related('books').annotate(num_books=Count('books')).all()
     return render(request, 'library/store_list.html', {'stores': stores})
 
 
 def store_detail(request, pk):
-    store = get_object_or_404(Store, pk=pk)
-    return render(request, 'library/store_detail.html', {'store': store})
+    store = get_object_or_404(Store.objects.prefetch_related('books'), pk=pk)
+    avg_pages = store.books.aggregate(Avg('pages'))['pages__avg']
+    return render(request, 'library/store_detail.html', {'store': store, 'avg_pages': avg_pages})
