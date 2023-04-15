bc9a550dcdc69f9e86802b507593469957ec182e
karabas94 <alkar_94@ukr.net>
2023-04-16 00:47:03 +0300

    -book create,update, delete views and templates created

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BookDetailView, BookListView
+from .views import BookDetailView, BookListView, BookCreateView, BookUpdateView, BookDeleteView
 
 from . import views
 
@@ -8,10 +8,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('authors/', views.author_list, name='author_list'),
     path('authors/<int:pk>/', views.author_detail, name='author_detail'),
-    # path('books/', views.book_list, name='book_list'),
-    # path('books/<int:pk>/', views.book_detail, name='book_detail'),
     path('books/', BookListView.as_view(), name='book_list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/create/', BookCreateView.as_view(), name='book_form'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
     path('publishers/', views.publisher_list, name='publisher_list'),
     path('publishers/<int:pk>/', views.publisher_detail, name='publisher_detail'),
     path('stores/', views.store_list, name='store_list'),
